b0f05b87ede2d25d2b2ca78173bb3060180140b3
sehatmaru <sehat.samosir@adapundi.com>
2023-10-02 17:27:01 +0700

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by AP on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
